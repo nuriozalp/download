0c94fc015a642c1249027ed6d96f7acb918cda0c
nuri <ozalpnuri@gmail.com>
2024-02-27 15:05:15 +0100

    new sciprt

---
+++ test/barcode.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+VENDOR_ID="1a86" # BARCODE cihazının vendor ID'si
+
+# /dev/ttyACM* ve /dev/ttyUSB* cihazları için döngü oluştur
+for DEVICE in /dev/ttyACM*; do
+    # Cihazın vendor ID'sini kontrol et
+    if udevadm info --query=property --name=$DEVICE | grep -q "ID_VENDOR_ID=$VENDOR_ID"; then
+        # Cihazın product ID'sini al
+        PRODUCT_ID=$(udevadm info --query=property --name=$DEVICE | grep "ID_MODEL_ID" | cut -d'=' -f2)
+        
+        if [ ! -z "$PRODUCT_ID" ]; then
+            echo "BARCODE device found: $DEVICE with PRODUCT_ID=$PRODUCT_ID"
+            
+            # Udev kuralını tanımla
+            RULE="SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"$VENDOR_ID\", ATTRS{idProduct}==\"$PRODUCT_ID\", TEST==\"power/control\", SYMLINK+=\"msfbarcode\""
+            
+            # Udev kuralını dosyaya yaz
+            echo "$RULE" > /etc/udev/rules.d/msf_barcode_card_setting.rules
+            
+            # Udev kurallarını yeniden yükle
+            echo "Reloading udev rules..."
+            udevadm control --reload-rules && udevadm trigger
+            
+            echo "Udev rule for BARCODE device $DEVICE with PRODUCT_ID=$PRODUCT_ID is created."
+            exit 0 # İlk uygun cihaz için kural oluşturulduktan sonra çık
+        fi
+    fi
+done
+
+echo "No BARCODE device with vendor ID $VENDOR_ID found. Exiting."
+exit 1
